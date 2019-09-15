a2043e31e13c874bf737ff43a754cdb977181067
fkwok <fkwok@homeaway.com>
2019-09-16 00:03:40 +1000

    adding mysql connection stuff

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
+$con = new mysqli("127.0.0.1", "root", "password23", "car_buddy");
+$users = $con->query("SELECT * FROM Users")->fetch_object()->users;
+$con->close();
+echo "$users <br/>";
+echo "Hello From Sites Folder!";
 phpinfo();
 ?>
