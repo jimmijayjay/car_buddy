cf2c3bc64156e391beb4cec6e8dce2f366c6285b
fkwok <fkwok@homeaway.com>
2019-09-16 14:33:08 +1000

    minor update

--- html/index.php
+++ html/index.php
@@ -1,8 +1,9 @@
 <?php
-$con = new mysqli("127.0.0.1", "root", "password23", "car_buddy");
-$users = $con->query("SELECT * FROM Users")->fetch_object()->users;
-$con->close();
+#$con = new mysqli("127.0.0.1", "root", "password23", "car_buddy");
+#$users = $con->query("SELECT * FROM Users")->fetch_object()->users;
+#$con->close();
 echo "$users <br/>";
-echo "Hello From Car Buddy!";
+echo "Hello From Car Buddy!<br/><br/>";
+
 phpinfo();
 ?>
