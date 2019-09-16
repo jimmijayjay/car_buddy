0b7c4770d7b0e30884f38ff27bc7b0d1900a4955
fkwok <fkwok@homeaway.com>
2019-09-16 12:25:37 +1000

    mindless update

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ $con = new mysqli("127.0.0.1", "root", "password23", "car_buddy");
 $users = $con->query("SELECT * FROM Users")->fetch_object()->users;
 $con->close();
 echo "$users <br/>";
-echo "Hello From Sites Folder!";
+echo "Hello From Car Buddy!";
 phpinfo();
 ?>
