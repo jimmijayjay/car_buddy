62463f285d9d472d0aeb66d3a88404e09f3f0a93
jimmijayjay <40873967+jimmijayjay@users.noreply.github.com>
2019-09-16 20:28:14 +1000

    Create tools.php
    Tools page to store common php calls

---
+++ html/tools.php
@@ -0,0 +1,32 @@
+
+function usernameDBConnect() {
+	<?php
+	$servername = "127.0.0.1";
+  $username = "root";
+  $password = "password23";
+  $dbname = "car_buddy";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  // Check connection
+  if ($conn->connect_error) {
+  	die("Connection failed: " . $conn->connect_error);
+  }
+}
+
+function usernameDBList() {
+	usernameDBConnect();
+  $sql = "SELECT UserID, FirstName, LastName, Age FROM Users";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+  	// output data of each row
+    while($row = $result->fetch_assoc()) {
+    	echo "id: " . $row["UserID"]. " - First Name: " . $row["FirstName"]. " " . $row["LastName"]. "<br>";
+    }
+	} else {
+  	echo "0 results";
+  }
+
+  $conn->close();
+}
