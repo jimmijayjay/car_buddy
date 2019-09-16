a12d7ab9b8106ffdfa576dc8d9b4ad3abf772b1e
fkwok <fkwok@homeaway.com>
2019-09-16 14:47:38 +1000

    adding an index file

--- html/index.php
+++ html/index.php
@@ -1,9 +1,21 @@
+<!doctype html>
+<html>
+<head>
+  <title>Welcome to Car Buddy!</title>
+</head>
+
+<body>
+  <p1>Welcome to Car Buddy</p1>
+
+  <?php phpinfo(); ?>
+</body>
+</html>
+
+
+
+
 <?php
 #$con = new mysqli("127.0.0.1", "root", "password23", "car_buddy");
 #$users = $con->query("SELECT * FROM Users")->fetch_object()->users;
 #$con->close();
-echo "$users <br/>";
-echo "Hello From Car Buddy!<br/><br/>";
-
-phpinfo();
 ?>
