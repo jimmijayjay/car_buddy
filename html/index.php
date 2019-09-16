75d282b4fecf8868b1072b5ca660984084bf9621
fkwok <fkwok@homeaway.com>
2019-09-16 15:00:12 +1000

    added mysql connection test

--- html/index.php
+++ html/index.php
@@ -7,15 +7,34 @@
 <body>
   <h1 style="text-align: center;">Welcome to Car Buddy</h1>
 
-  <?php phpinfo(); ?>
+  <?php
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "password23";
+    $dbname = "car_buddy";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT UserID, FirstName, LastName, Age FROM Users";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["UserID"]. " - First Name: " . $row["FirstName"]. " " . $row["LastName"]. "<br>";
+      }
+    } else {
+      echo "0 results";
+    }
+    $conn->close();
+
+    phpinfo();
+  ?>
 </body>
 </html>
-
-
-
-
-<?php
-#$con = new mysqli("127.0.0.1", "root", "password23", "car_buddy");
-#$users = $con->query("SELECT * FROM Users")->fetch_object()->users;
-#$con->close();
-?>
